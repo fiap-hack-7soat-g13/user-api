07d4dbdb53535b0886b1e0f3415c1cfe5458ed3f
Cristiano de Barros <cristiano.barros13@gmail.com>
2025-02-08 17:51:29 -0300

    Corrigindo projeto para realizar o build

--- src/main/java/com/fiap/challenge/user/app/adapter/output/persistence/entity/UserEntity.java
+++ src/main/java/com/fiap/challenge/user/app/adapter/output/persistence/entity/UserEntity.java
@@ -3,22 +3,23 @@ package com.fiap.challenge.user.app.adapter.output.persistence.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "user", schema = "public")
+@Table(name = "users")
 @Builder(toBuilder = true)
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String document;
 
 }
